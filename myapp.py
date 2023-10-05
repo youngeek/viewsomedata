fa2d585f6df9ab9f69db8411f623053c17d85c60
Daniel Figuredo <jdanielll@hotmail.com>
2023-10-05 17:19:10 -0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,10 +1,8 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("Hamlet said…")
-st.text("""
-To be, or not to be, that is the question:
-Whether 'tis nobler in the mind to suffer
-The slings and arrows of outrageous fortune,
-Or to take arms against a sea of troubles
-And by opposing end them.
-""")
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
+
