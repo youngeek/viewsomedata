108ea63320c47b1a0b96711d761ffbe6131f81e5
Daniel Figuredo <jdanielll@hotmail.com>
2023-10-05 17:20:40 -0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+chart_data = pd.read_csv('counts.csv')
 
 st.bar_chart(chart_data)
 
