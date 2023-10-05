b4d2991aaecf10524f7843736ce1a17200e3f9ba
Daniel Figuredo <jdanielll@hotmail.com>
2023-10-05 17:14:11 -0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,1 +1,10 @@
-print("hello")
+import streamlit as st
+
+st.title("Hamlet said…")
+st.text("""
+To be, or not to be, that is the question:
+Whether 'tis nobler in the mind to suffer
+The slings and arrows of outrageous fortune,
+Or to take arms against a sea of troubles
+And by opposing end them.
+""")
