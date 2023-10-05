a218c58300c9e9c2530658352ec968ce1a3611f4
Daniel Figuredo <jdanielll@hotmail.com>
2023-10-05 17:29:39 -0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-chart_data = pd.read_csv('counts.csv')
+chart_data = pd.read_csv('counts.csv', columns=['dia3','dia4','dia5'])
 
-st.bar_chart(chart_data, columns=['dia3','dia4','dia5'], x='device_id')
+st.bar_chart(chart_data, x='device_id')
 
