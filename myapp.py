7a76be92a96444c707c448cf10026df67518da8c
Daniel Figuredo <jdanielll@hotmail.com>
2023-10-05 17:34:22 -0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,8 +1,39 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
 
-chart_data = pd.read_csv('counts.csv', columns=['dia3','dia4','dia5'])
+# Load the CSV file into a Pandas DataFrame
+@st.cache
+def load_data():
+    df = pd.read_csv('data.csv')
+    return df
 
-st.bar_chart(chart_data, x='device_id')
+df = load_data()
 
+# Set the page title
+st.title("Bar Chart Example with Streamlit")
+
+# Create a sidebar for user input (optional)
+st.sidebar.header("Configuration")
+
+# Select the column for x-axis (device_id)
+x_axis_column = st.sidebar.selectbox("Select X-Axis Column:", df.columns, index=0)
+
+# Select the columns for the bar chart
+bar_columns = st.sidebar.multiselect("Select Bar Columns:", df.columns[1:], default=['dia3'])
+
+# Create the bar chart
+st.write(f"### Bar Chart - X-Axis: {x_axis_column}, Bars: {', '.join(bar_columns)}")
+
+fig, ax = plt.subplots()
+for col in bar_columns:
+    ax.bar(df[x_axis_column], df[col], label=col)
+
+# Set labels and title
+ax.set_xlabel(x_axis_column)
+ax.set_ylabel("Values")
+ax.set_title("Bar Chart")
+ax.legend()
+
+# Display the chart using Streamlit
+st.pyplot(fig)
