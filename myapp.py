7bd895566a9440200a8a81779dd5461e91610077
Daniel Figuredo <jdanielll@hotmail.com>
2023-10-05 17:42:44 -0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -30,7 +30,6 @@ bar_data = df.groupby(x_axis_column)[bar_columns].sum()
 # Plot the bar chart using Streamlit
 st.bar_chart(bar_data)
 
-'''------line chart----------'''
 # Set the page title
 st.title("Line Chart Example with Streamlit")
 
@@ -38,16 +37,16 @@ st.title("Line Chart Example with Streamlit")
 st.sidebar.header("Configuration")
 
 # Select the column for x-axis (device_id)
-x_axis_column = st.sidebar.selectbox("Select X-Axis Column:", df.columns, index=0)
+x_axis_column_line = st.sidebar.selectbox("Select X-Axis Column Line Chart:", df.columns, index=0)
 
 # Select the columns for the line chart
-line_columns = st.sidebar.multiselect("Select Line Columns:", df.columns[1:], default=['dia3'])
+line_columns_line = st.sidebar.multiselect("Select Line Columns Line Chart:", df.columns[1:], default=['dia3'])
 
 # Create the line chart using Streamlit
-st.write(f"### Line Chart - X-Axis: {x_axis_column}, Lines: {', '.join(line_columns)}")
+st.write(f"### Line Chart - X-Axis: {x_axis_column_line}, Lines: {', '.join(line_columns_line)}")
 
 # Group by the selected x-axis column and calculate the sum of the selected line columns
-line_data = df.groupby(x_axis_column)[line_columns].sum()
+line_data = df.groupby(x_axis_column_line)[line_columns_line].sum()
 
 # Plot the line chart using Streamlit
 st.line_chart(line_data)
